2f1bb802220dfe908e72f5122b59b2e8c1cfa7ea
Ceskilia <78687302+Ceskilia@users.noreply.github.com>
2025-05-12 14:49:20 +0200

    Add missing annotation

--- backend/src/main/java/com/backend/service/parser/DVDImportParser.java
+++ backend/src/main/java/com/backend/service/parser/DVDImportParser.java
@@ -1,15 +1,17 @@
 package com.backend.service.parser;
 
+import org.springframework.stereotype.Service;
+
 import com.backend.entity.Produkt;
 import com.backend.service.dto.ItemData;
 import com.backend.service.util.Result;
 
+@Service
 public class DVDImportParser extends ProduktImportParser {
 
     @Override
     public Result<? extends Produkt> parseProdukt(ItemData itemData) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'parseProdukt'");
+        return Result.error("Not implemented."); // TBD
     }
 
 }
